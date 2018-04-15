3df695e203245051fa316bee251095baeb739285
Danil Tolonbekov <tolonbekov@yandex.com>
2018-04-15 13:40:23 +0600

    Set UUID

--- app/src/main/java/com/example/t/myapplication/MainActivity.java
+++ app/src/main/java/com/example/t/myapplication/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.t.myapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.UUID;
+
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    //public UUID HEART_RATE_SERVICE_UUID = convertFromInteger(0x180D);
+    //public UUID HEART_RATE_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A37);
+    //public UUID HEART_RATE_CONTROL_POINT_CHAR_UUID = convertFromInteger(0x2A39);
+    public static UUID ECG_SENSOR = UUID.fromString("ade7a273-89f9-49e1-b9d4-3cb36bce261b");
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public UUID convertFromInteger(int i) {
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
+    }
 }
+
+
+
+
