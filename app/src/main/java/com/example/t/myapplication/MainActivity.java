9b19b9336aa043a0040cb7a72428767f98a897f8
Danil Tolonbekov <tolonbekov@yandex.com>
2018-04-24 21:59:19 +0600

    Set UUID

--- app/src/main/java/com/example/t/myapplication/MainActivity.java
+++ app/src/main/java/com/example/t/myapplication/MainActivity.java
@@ -1,40 +1,130 @@
 package com.example.t.myapplication;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
-
-
+@TargetApi(Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_FINE_LOCATION = 2;
 
-    //public UUID HEART_RATE_SERVICE_UUID = convertFromInteger(0x180D);
-    //public UUID HEART_RATE_MEASUREMENT_CHAR_UUID = convertFromInteger(0x2A37);
+    public UUID HEART_RATE_SERVICE_UUID = UUID.fromString("5b552788-5c7b-4ce8-8362-Cf5dd093251d");
+    public UUID HEART_RATE_MEASUREMENT_CHAR_UUID = UUID.fromString("ade7a273-89f9-49e1-b9d4-3cb36bce261b");
     //public UUID HEART_RATE_CONTROL_POINT_CHAR_UUID = convertFromInteger(0x2A39);
-    public static UUID ECG_SENSOR = UUID.fromString("ade7a273-89f9-49e1-b9d4-3cb36bce261b");
+    //public static UUID ECG_SENSOR = UUID.fromString("ade7a273-89f9-49e1-b9d4-3cb36bce261b");
+    private Handler mHandler;
+    private Map<String, BluetoothDevice> mScanResults;
 
-    private BluetoothAdapter bluetoothAdapter;
+    private boolean mConnected;
+    private boolean mEchoInitialized;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private ScanCallback mScanCallback;
+    private BluetoothGatt mGatt;
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
 
-    final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+    }
+
+    private void startScan() {
+        if (!hasPermissions() || mScanning) {
+            return;
+        }
+        // TODO start the scan
+        List<ScanFilter> filters = new ArrayList<>();
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .build();
+        mScanResults = new HashMap<>();
+        mScanCallback = new BtleScanCallback(mScanResults);
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+        mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
+        mScanning = true;
+        mHandler = new Handler();
+        //mHandler.postDelayed(this::stopScan, SCAN_PERIOD);
+    }
+
+    private class BtleScanCallback extends ScanCallback {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            addScanResult(result);
+        }
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult result : results) {
+                addScanResult(result);
+            }
+        }
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e(TAG, "BLE Scan Failed with code " + errorCode);
+        }
+        private void addScanResult(ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            String deviceAddress = device.getAddress();
+            mScanResults.put(deviceAddress, device);
+        }
+    };
+
+    private boolean hasPermissions() {
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            requestBluetoothEnable();
+            return false;
+        } else if (!hasLocationPermissions()) {
+            requestLocationPermission();
+            return false;
+        }
+        return true;
+    }
+    private void requestBluetoothEnable() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        Log.d(TAG, "Requested user enables Bluetooth. Try starting the scan again.");
+    }
+
+    private boolean hasLocationPermissions() {
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestLocationPermission() {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
     }
 
 
-    public UUID convertFromInteger(int i) {
-        final long MSB = 0x0000000000001000L;
-        final long LSB = 0x800000805f9b34fbL;
-        long value = i & 0xFFFFFFFF;
-        return new UUID(MSB | (value << 32), LSB);
-    }
+
+
 }
 
 
